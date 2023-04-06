f46776a2930989db35270b4114e4c28309d36bdf
javierlopezfernandez <javier.l@thoughtworks.com>
2023-04-06 13:31:40 +0200

    extrae el marsView a su propio fichero

--- src/components/maps/rover/MarsView.test.tsx
+++ src/components/maps/rover/MarsView.test.tsx
@@ -1,58 +1,8 @@
 import {render, screen} from "@testing-library/react";
 import spyOn = jest.spyOn;
 import * as dimensions from "../../../dimensions/map-dimensions";
-import {useDimensions} from "../../../dimensions/map-dimensions";
-import surface from "./mars-surface.jpeg";
-import rover from "./rover.png";
+import {MarsView} from "./MarsView";
 
-interface MarsRowProps {
-    rowNumber: number;
-    cols: number;
-}
-
-const MarsRow = ({rowNumber, cols}: MarsRowProps) => {
-    let rangeForSurfaces = Array.from(Array(cols).keys()).map(x => x + 1);
-    if (rowNumber === 1) {
-        rangeForSurfaces.shift();
-    }
-    return (
-        <div aria-label={"Row"}>
-            {rowNumber === 1 && <RoverElement/>}
-            {
-                rangeForSurfaces.map((col) => <SurfaceElement key={col}/>)
-            }
-        </div>
-    )
-}
-const SurfaceElement = (): JSX.Element => (
-    <div>
-        <img
-            src={surface}
-            alt="Mars surface"
-            className="img-surface"
-        />
-    </div>
-);
-const RoverElement = (): JSX.Element => (
-    <div>
-        <img
-            src={rover}
-            alt="Mars Rover"
-            className="img-rover"
-        />
-    </div>
-);
-const MarsView = (): JSX.Element => {
-    const {height, width} = useDimensions();
-    return (
-        <div>
-            {
-                Array.from(Array(height).keys()).map(x => x + 1).map(row => <MarsRow key={row} cols={width}
-                                                                                     rowNumber={row}/>)
-            }
-        </div>
-    );
-}
 const mockedUseDimensions = spyOn(dimensions, "useDimensions");
 describe("MarsView", () => {
     it("renders exactly one rover withing the surface", async () => {
