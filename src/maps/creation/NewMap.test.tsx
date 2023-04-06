c279ca4650ba214c7a2b6149462629b809228623
javierlopezfernandez <javier.l@thoughtworks.com>
2023-04-06 09:57:06 +0200

    Version muy simple para redirigirnos al mapa

--- src/maps/creation/NewMap.test.tsx
+++ src/maps/creation/NewMap.test.tsx
@@ -1,21 +1,43 @@
 import React from 'react';
-import {render, screen} from "@testing-library/react";
+import {fireEvent, render, screen} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import {NewMap} from "./NewMap";
 import {getMapDimensions} from "../dimensions/map-dimensions";
+import {createMemoryRouter, RouterProvider} from "react-router-dom";
+
+function renderWithRouter(element: JSX.Element) {
+    const newMapRouter = createMemoryRouter([{
+        path: "/",
+        element: element
+    }, {
+        path: "/map",
+        element: <p>Estamos en el mapa del marsRover</p>
+    }]);
+    render(
+        <RouterProvider router={newMapRouter}/>
+    );
+}
 
 describe('NewMap', function () {
     it('should ask for map dimensions', function () {
-        render(<NewMap/>);
+        renderWithRouter(<NewMap/>);
         expect(screen.getByText("¿Cual será la altura de nuestro mapa?")).toBeInTheDocument();
         expect(screen.getByText("¿Cual será la anchura de nuestro mapa?")).toBeInTheDocument();
     });
 
     it('should change map dimensions when user introduce them', function () {
-        render(<NewMap/>);
+        renderWithRouter(<NewMap/>);
         userEvent.type(screen.getByLabelText("¿Cual será la altura de nuestro mapa?"), "1");
         userEvent.type(screen.getByLabelText("¿Cual será la anchura de nuestro mapa?"), "2");
         expect(getMapDimensions().height).toEqual(1);
         expect(getMapDimensions().width).toEqual(2);
+    });
+
+    it('should redirect to the page that paints the rovers in the map', function () {
+        renderWithRouter(<NewMap/>);
+        userEvent.type(screen.getByLabelText("¿Cual será la altura de nuestro mapa?"), "1");
+        userEvent.type(screen.getByLabelText("¿Cual será la anchura de nuestro mapa?"), "2");
+        fireEvent.click(screen.getByText("Juguemos"));
+        expect(screen.getByText("Estamos en el mapa del marsRover")).toBeInTheDocument();
     });
 });
--- src/maps/creation/NewMap.tsx
+++ src/maps/creation/NewMap.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {changeHeight, changeWidth} from "../dimensions/map-dimensions";
+import {Link} from "react-router-dom";
 
 export function NewMap() {
     return (
@@ -8,6 +9,7 @@ export function NewMap() {
             <input id={"height"} type="text" onChange={(e) => changeHeight(e.target.value)}/>
             <label htmlFor={"anchura"}>¿Cual será la anchura de nuestro mapa?</label>
             <input id={"anchura"} type="text" onChange={(e) => changeWidth(e.target.value)}/>
+            <Link to={"/map"}>Juguemos</Link>
         </div>
     );
 }
