356576dea61154c38e1abf6023fdfe888d8147e9
javierlopezfernandez <javier.l@thoughtworks.com>
2023-04-07 08:03:27 +0200

    valida múltiples superficies con el rover dentro de ellas

--- src/components/maps/rover/MarsView.test.tsx
+++ src/components/maps/rover/MarsView.test.tsx
@@ -4,19 +4,25 @@ import * as dimensions from "../../../dimensions/map-dimensions";
 import {MarsView} from "./MarsView";
 
 const mockedUseDimensions = spyOn(dimensions, "useDimensions");
+
+function mockDimensions(height: number, width: number) {
+    mockedUseDimensions.mockReturnValue({
+        height: height,
+        width: width,
+        isCompleted: () => true
+    });
+}
+
 describe("MarsView", () => {
-    it("renders exactly one rover withing the surface", async () => {
-        mockedUseDimensions.mockReturnValue({
-            height: 6,
-            width: 5,
-            isCompleted: () => true
-        });
+    it.each([{height:1, width:1}, {height: 2, width: 2}, {height: 6, width: 5}])
+    ("renders exactly one rover withing the surface of dimensions %p",  ({height, width}) => {
+        mockDimensions(height, width);
         render(<MarsView/>);
         const rows = screen.queryAllByLabelText(/Row/);
-        expect(rows.length).toBe(6);
+        expect(rows.length).toBe(height);
         const surfaceElements = screen.queryAllByAltText(/Mars surface/);
-        expect(surfaceElements.length).toBe(29);
-        const rover = await screen.findAllByAltText(/Mars Rover/);
+        expect(surfaceElements.length).toBe(height*width-1);
+        const rover =  screen.queryAllByAltText(/Mars Rover/);
         expect(rover.length).toBe(1);
     });
 });
