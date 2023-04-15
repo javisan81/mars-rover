66ea406c8555e6764ec7f8aaf73fb540e1c58f06
javierlopezfernandez <javier.l@thoughtworks.com>
2023-04-15 17:36:36 +0200

    fix bug with the hook, needs to remember the marsRover

--- src/marsRover/MarsRoverHook.test.ts
+++ src/marsRover/MarsRoverHook.test.ts
@@ -1,16 +1,24 @@
 import {useMarsRover} from "./MarsRoverHook";
-import {Direction} from "./MarsRoverInterface";
+import {Direction, MoveCommand} from "./MarsRoverInterface";
+import {renderHook} from "@testing-library/react";
 
 describe('MarsRoverHook', function () {
     it('should return mars rover facing west in 1,1', function () {
-        const marsRover = useMarsRover(1,1);
-        expect(marsRover.getPosition()).toEqual({row:1, col:1});
+        const marsRover = renderHook(() => useMarsRover(1, 1)).result.current;
+        expect(marsRover.getPosition()).toEqual({row: 1, col: 1});
         expect(marsRover.getDirection()).toEqual(Direction.West);
     });
     it('should return mars rover in a map of dimensions 2,2', function () {
-        const marsRover = useMarsRover(2,2);
-        expect(marsRover.getPosition()).toEqual({row:1, col:1});
+        const marsRover = renderHook(() => useMarsRover(2, 2)).result.current;
+        expect(marsRover.getPosition()).toEqual({row: 1, col: 1});
         expect(marsRover.getDirection()).toEqual(Direction.West);
-        expect(marsRover.getMap()).toEqual({maxHeight:2, maxWidth:2})
+        expect(marsRover.getMap()).toEqual({maxHeight: 2, maxWidth: 2})
+    });
+    it('should reuse the same rover', function () {
+        const {result, rerender} = renderHook(() => useMarsRover(2, 2));
+        result.current.move([MoveCommand.Left]);
+        rerender();
+        result.current.move([MoveCommand.Left]);
+        expect(result.current.getDirection()).toEqual(Direction.East);
     });
 });
--- src/marsRover/MarsRoverHook.ts
+++ src/marsRover/MarsRoverHook.ts
@@ -1,7 +1,13 @@
 import {Direction, MarsRoverUseCase} from "./MarsRoverInterface";
 import {MarsRover, PositionDirection} from "./MarsRover";
+import {useMemo} from "react";
 
 
 export const useMarsRover = (height: number, width: number): MarsRoverUseCase => {
-    return new MarsRover(new PositionDirection({row: 1, col: 1}, Direction.West), {maxHeight: height, maxWidth: width});
+    return useMemo(() => {
+        return new MarsRover(new PositionDirection({row: 1, col: 1}, Direction.West), {
+            maxHeight: height,
+            maxWidth: width
+        });
+    }, [height, width]);
 }
